bf022bc098a8be1c1f4065f51232fdf9819d489b
Patty <pattychen.peiti@gmail.com>
2023-10-06 22:56:23 -0400

    w1

---
+++ WS01/Workshop1/w1p1/w1p1.c
@@ -0,0 +1,31 @@
+/* 
+*****************************************************************************
+<assessment name example : Workshop - #1 (Part - 1)>
+	Full Name : Pei-Ti, Chen
+	Student ID# :133237222
+	Email : pchen81@myseneca.ca
+	Section :NAA
+	Authenticity Declaration :
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider.This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/ 
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Workshop 1 Part-1\n"
+		   "=================\n\n"
+		   "I'm displaying this using the 'printf' stdio\n"
+		   "(standard input output) library function!\n\n"
+		   "Dear teacher,\n\n"
+		   "  I promise I will work hard from this day onward.\n"
+		   "  I acknowledge that practice is extremely important,\n"
+		   "  so I will do all workshops, quizzes, and assignments.\n\n"
+		   "Sincerely,\n\n"
+		   "Pei-Ti, Chen\n"
+		   "Student ID# 133237222");
+	return 0;
+}
---
+++ WS01/Workshop1/w1p1/w1p2.c
@@ -0,0 +1,41 @@
+/*
+*****************************************************************************
+<assessment name example : Workshop - #1 (Part - 1)>
+	Full Name : Pei-Ti, Chen
+	Student ID# :133237222
+	Email : pchen81@myseneca.ca
+	Section :NAA
+	Authenticity Declaration :
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider.This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Workshop 1 Part-2\n"
+		   "=================\n\n");
+
+	printf("Using tab specifiers...\n"
+		   "	Tab-1	Tab-2	Tab-3\n"
+		   "	-----	=====	-----\n\n");
+	 
+	printf("\\ this is a back-slash character!\n"
+		   "%% this is a percent sign character!\n"
+		   "\" this is a double-quote character!\n\n");
+
+	printf("My favourite quotes are:\n"
+		   "1.	\"Skill is only developed by hours and hours of work.\"\n"
+		   "						-Usain Bolt\n\n");
+
+	printf("2.	\"It's not about having time. It's about making time.\"\n"
+		   "						-unknown\n\n");
+
+	printf("3.	\"All of us do not have equal talent. But, all of us\n"
+		   "	have an equal opportunity to develop our talents.\"\n"
+		   "						-A.P.J. Abdul Kalam\n");
+	return 0;
+}
